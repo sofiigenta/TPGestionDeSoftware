8664f2d17aa390ff8dcf015acdcff1d8c7091b50
SG <sofiigenta@gmail.com>
2021-09-09 13:04:45 -0300

    Commit inicial

--- TP Gestion de Software.py
+++ TP Gestion de Software.py
@@ -1,9 +1,7 @@
+#Se desea conocer el aumento de salario de todos los vendedores de un local comercial, para lo cual deberá preguntar al operador el sueldo que se venía persibiendo, teiendo en cuenta que el aumento es del 25%. 
+#Se desea conocer cuánto ganará cada uno.
+#Además será necesario mostrar lo que la empresa deberá pagar a todos los vendedores en concepto de salario y con cuántos vendedores cuenta.
 
 
-contVendedor = 0
-acumuladorDia = 0
 
-
-
-v = int (input ("Ingesar 1 para registrar un vendedor o 0 para terminar \n"))
 
